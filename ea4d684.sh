d265ca01337247a6cbfde64834020fc3af624977
Ryan Kitty <ryan@kittydiaz.com>
2025-07-26 10:02:54 -0500

    Websites experiencing 421 Misdirected requests after upgrading to CloudLinux's ea-apache24-2.4.64

---
+++ ea4d684.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Websites experiencing 421 Misdirected requests after upgrading to CloudLinux's ea-apache24-2.4.64 
+echo "Beginning Downgrade..."
+yum downgrade -y liblsapi liblsapi-devel ea-apache24*
+echo "Installing versionlock..."
+dnf install -y python3-dnf-plugin-versionlock
+echo "Version locking affected packages..."
+yum versionlock -y liblsapi liblsapi-devel ea-apache24*
+echo "Completed"
